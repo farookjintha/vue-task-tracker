26216082885eb10f6a5751026a38591a5ed4b48f
farookjintha <farookjintha@gmail.com>
2022-04-18 22:36:19 +0530

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,97 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container">
+    <Header title="Task Tracker" />
+    <Tasks :tasks="tasks" @delete-task="deleteTask" />
+  </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+  import Header from './components/Header';
+  import Tasks from './components/Tasks';
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      Header,
+      Tasks
+    },
+    data(){
+      return {
+        tasks: []
+      }
+    },
+    methods: {
+      deleteTask(id){
+        if(confirm('Are you sure?')){
+          this.tasks = this.tasks.filter(task => task.id !== id);
+        }
+      }
+    },
+    created() {
+      this.tasks = [
+        {
+          id: 1,
+          text: 'Doctors Appointment',
+          day: 'March 1st at 2:30pm',
+          reminder: true
+        },
+        {
+          id: 2,
+          text: 'Badminton Game',
+          day: 'March 1st at 5:30pm',
+          reminder: true
+        },
+        {
+          id: 3,
+          text: 'Yoga Session',
+          day: 'March 1st at 8:30pm',
+          reminder: false
+        }
+      ]
+    }
   }
-})
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-family: 'Poppins', sans-serif;
+  }
+  .container {
+    max-width: 500px;
+    margin: 30px auto;
+    overflow: auto;
+    min-height: 300px;
+    border: 1px solid steelblue;
+    padding: 30px;
+    border-radius: 5px;
+  }
+  .btn {
+    display: inline-block;
+    background: #000;
+    color: #fff;
+    border: none;
+    padding: 10px 20px;
+    margin: 5px;
+    border-radius: 5px;
+    cursor: pointer;
+    text-decoration: none;
+    font-size: 15px;
+    font-family: inherit;
+  }
+  .btn:focus {
+    outline: none;
+  }
+  .btn:active {
+    transform: scale(0.98);
+  }
+  .btn-block {
+    display: block;
+    width: 100%;
+  }
 </style>
